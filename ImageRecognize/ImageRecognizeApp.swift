924aeb819dfd955114e48d6407c924dc35292e7d
Victor Brigido <victorbrigido@icloud.com>
2024-08-08 09:07:46 -0300

    Initial Commit

---
+++ ImageRecognize/ImageRecognizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageRecognizeApp.swift
+//  ImageRecognize
+//
+//  Created by Victor Brigido on 08/08/24.
+//
+
+import SwiftUI
+
+@main
+struct ImageRecognizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
